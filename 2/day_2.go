0b7b8926cd375e9caa28d7ed6e3cff2572e76e2b
Sérgio Vitarelli Silva <sergiovitarellisilva@gmail.com>
2025-09-18 21:58:47 -0300

    implements brute force solution

--- 2/day_2.go
+++ 2/day_2.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 )
 
-//Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
-//
-//For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
-//
-//Follow-up: what if you can't use division?
+/*Given an array of integers, return a new array such that each element at
+index i of the new array is the product of all the numbers in the original
+array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be
+[120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would
+be [2, 3, 6].
+
+Follow-up: what if you can't use division?*/
 
 func multiplesList(list []int) []int {
 	result := make([]int, len(list))
@@ -25,12 +29,25 @@ func multiplesList(list []int) []int {
 	return result
 }
 
+// Brute force. Non optimized.
 func multiplesListWithoutDivision(list []int) []int {
-	return []int{0}
+	result := make([]int, len(list))
+
+	for i := range list {
+		tmp := 1
+		for j := range list {
+			if i != j {
+				tmp *= list[j]
+			}
+		}
+		result[i] = tmp
+	}
+
+	return result
 }
 
 func main() {
 	list := []int{1, 2, 3, 4, 5}
 	fmt.Println(multiplesList(list))
-	//multiplesListWithoutDivision(list)
+	fmt.Println(multiplesListWithoutDivision(list))
 }
